aaa51aa35da5ade5f42f76f9d8aa230c97246d11
hhzh <1138253815@qq.com>
2016-09-16 22:31:14 +0800

    修正一些错误

--- src/main/java/com/nile/controller/UserContoller.java
+++ src/main/java/com/nile/controller/UserContoller.java
@@ -3,16 +3,16 @@ package com.nile.controller;
 
 import com.nile.entity.User;
 import com.nile.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
 public class UserContoller extends BaseController{
 
+    @Autowired
     private UserService userService;
 
     @RequestMapping(value = "/select",method = RequestMethod.GET)
