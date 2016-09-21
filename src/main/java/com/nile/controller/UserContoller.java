ea1dfe8f90f025b0adc093e37a5d80ea8b011acd
hhzh <1138253815@qq.com>
2016-09-21 23:01:27 +0800

    test

--- src/main/java/com/nile/controller/UserContoller.java
+++ src/main/java/com/nile/controller/UserContoller.java
@@ -1,30 +1,25 @@
 package com.nile.controller;
 
 
-import com.nile.entity.User;
-import com.nile.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
-public class UserContoller extends BaseController{
+public class UserContoller{
 
-    @Autowired
-    private UserService userService;
+//    @Autowired
+//    private UserService userService;
 
-    @RequestMapping(value = "/select",method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity<String> selectUserById() {
-        User user=userService.selectUserById(1000);
-        System.out.println(user.getName());
+    @RequestMapping(value = "/select")
+    public String  selectUserById() {
+//        User user=userService.selectUserById(1000);
+//        System.out.println(user.getName());
         //return new ResponseEntity<String>("abc", HttpStatus.OK);
 
-        return response("user",user);
+        System.out.println("Hello.");
+//        return response("user",user);
+        return "hello";
     }
 
 }
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: a
+  Date: 2016/9/21
+  Time: 22:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello page</title>
+</head>
+<body>
+    <h2>Hello springmvc.</h2>
+</body>
+</html>
