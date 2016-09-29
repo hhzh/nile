1d10d397fed794cfd8d63ede1d52a68f216269bc
hehuazhen <hehuazhen@shbj.com>
2016-09-29 20:25:11 +0800

    重新重构项目

--- src/main/java/com/nile/entity/User.java
+++ src/main/java/com/nile/entity/User.java
@@ -1,15 +1,9 @@
 package com.nile.entity;
 
-import java.util.Date;
-
 public class User {
+
     private Integer id;
     private String name;
-    private String phone;
-    private Date createTime;
-    private Date updateTime;
-
-    private Book book;
 
     public Integer getId() {
         return id;
@@ -27,47 +21,11 @@ public class User {
         this.name = name;
     }
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Book getBook() {
-        return book;
-    }
-
-    public void setBook(Book book) {
-        this.book = book;
-    }
-
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", phone='" + phone + '\'' +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                ", book=" + book +
                 '}';
     }
 }
