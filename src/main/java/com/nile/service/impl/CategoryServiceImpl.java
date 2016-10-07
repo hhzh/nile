0ae6c1e57c36b353cff67c3d3d0aecdcae3b590f
hhzh <1138253815@qq.com>
2016-10-07 09:35:48 +0800

    增加类别

--- src/main/java/com/nile/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/nile/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,33 @@
 package com.nile.service.impl;
 
+import com.nile.entity.Category;
 import com.nile.service.CategoryService;
 
+import java.util.List;
+
 public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public Category selectCategoryById(int id) {
+        return null;
+    }
+
+    @Override
+    public void insertCategory(Category category) {
+
+    }
+
+    @Override
+    public void updateCategory(Category ctegory) {
+
+    }
+
+    @Override
+    public void deleteCategoryById(Integer id) {
+
+    }
+
+    @Override
+    public List<Category> selectAll() {
+        return null;
+    }
 }
