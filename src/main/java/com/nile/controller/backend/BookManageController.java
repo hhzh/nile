9de95029dee3b0acd58522a7cb40615e351e070f
hhzh <1138253815@qq.com>
2017-05-29 22:11:14 +0800

    增加前端接口

---
+++ src/main/java/com/nile/controller/backend/BookManageController.java
@@ -0,0 +1,205 @@
+package com.nile.controller.backend;
+
+
+import com.google.common.collect.Maps;
+import com.nile.common.Const;
+import com.nile.common.ResponseCode;
+import com.nile.common.ServerResponse;
+import com.nile.pojo.Book;
+import com.nile.pojo.User;
+import com.nile.service.IBookService;
+import com.nile.service.IFileService;
+import com.nile.service.IUserService;
+import com.nile.util.PropertiesUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/manage/book")
+public class BookManageController {
+
+    @Autowired
+    private IUserService userService;
+    @Autowired
+    private IBookService bookService;
+    @Autowired
+    private IFileService iFileService;
+
+    @RequestMapping("save")
+    @ResponseBody
+    public ServerResponse bookSave(HttpSession session, Book book){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            //填充我们增加产品的业务逻辑
+            return bookService.saveOrUpdateBook(book);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("set_sale_status")
+    @ResponseBody
+    public ServerResponse setSaleStatus(HttpSession session, Integer bookId,Integer status){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            return bookService.setSaleStatus(bookId,status);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("detail")
+    @ResponseBody
+    public ServerResponse getDetail(HttpSession session, Integer bookId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            //填充业务
+            return bookService.manageBookDetail(bookId);
+
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("list")
+    @ResponseBody
+    public ServerResponse getList(HttpSession session, @RequestParam(value = "pageNum",defaultValue = "1") int pageNum, @RequestParam(value = "pageSize",defaultValue = "10") int pageSize){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            //填充业务
+            return bookService.getBookList(pageNum,pageSize);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("search")
+    @ResponseBody
+    public ServerResponse bookSearch(HttpSession session,String bookName,Integer bookId, @RequestParam(value = "pageNum",defaultValue = "1") int pageNum,@RequestParam(value = "pageSize",defaultValue = "10") int pageSize){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            //填充业务
+            return bookService.searchBook(bookName,bookId,pageNum,pageSize);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("upload")
+    @ResponseBody
+    public ServerResponse upload(HttpSession session, @RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file,path);
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
+
+            Map fileMap = Maps.newHashMap();
+            fileMap.put("uri",targetFileName);
+            fileMap.put("url",url);
+            return ServerResponse.createBySuccess(fileMap);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+
+    @RequestMapping("richtext_img_upload")
+    @ResponseBody
+    public Map richtextImgUpload(HttpSession session, @RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request, HttpServletResponse response){
+        Map resultMap = Maps.newHashMap();
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            resultMap.put("success",false);
+            resultMap.put("msg","请登录管理员");
+            return resultMap;
+        }
+        //富文本中对于返回值有自己的要求,我们使用是simditor所以按照simditor的要求进行返回
+//        {
+//            "success": true/false,
+//                "msg": "error message", # optional
+//            "file_path": "[real file path]"
+//        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file,path);
+            if(StringUtils.isBlank(targetFileName)){
+                resultMap.put("success",false);
+                resultMap.put("msg","上传失败");
+                return resultMap;
+            }
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
+            resultMap.put("success",true);
+            resultMap.put("msg","上传成功");
+            resultMap.put("file_path",url);
+            response.addHeader("Access-Control-Allow-Headers","X-File-Name");
+            return resultMap;
+        }else{
+            resultMap.put("success",false);
+            resultMap.put("msg","无权限操作");
+            return resultMap;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/nile/controller/backend/OrderManageController.java
@@ -0,0 +1,110 @@
+package com.nile.controller.backend;
+
+
+import com.github.pagehelper.PageInfo;
+import com.nile.VO.OrderVO;
+import com.nile.common.Const;
+import com.nile.common.ResponseCode;
+import com.nile.common.ServerResponse;
+import com.nile.pojo.User;
+import com.nile.service.IOrderService;
+import com.nile.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/manage/order")
+public class OrderManageController {
+
+    @Autowired
+    private IUserService userService;
+    @Autowired
+    private IOrderService orderService;
+
+    @RequestMapping("list")
+    @ResponseBody
+    public ServerResponse<PageInfo> orderList(HttpSession session, @RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
+                                              @RequestParam(value = "pageSize",defaultValue = "10")int pageSize){
+
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            //填充我们增加产品的业务逻辑
+            return orderService.manageList(pageNum,pageSize);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("detail")
+    @ResponseBody
+    public ServerResponse<OrderVO> orderDetail(HttpSession session, Long orderNo){
+
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            //填充我们增加产品的业务逻辑
+
+            return orderService.manageDetail(orderNo);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+
+
+    @RequestMapping("search")
+    @ResponseBody
+    public ServerResponse<PageInfo> orderSearch(HttpSession session, Long orderNo,@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
+                                               @RequestParam(value = "pageSize",defaultValue = "10")int pageSize){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            //填充我们增加产品的业务逻辑
+            return orderService.manageSearch(orderNo,pageNum,pageSize);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+
+
+    @RequestMapping("send_goods")
+    @ResponseBody
+    public ServerResponse<String> orderSendGoods(HttpSession session, Long orderNo){
+
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(userService.checkAdminRole(user).isSuccess()){
+            //填充我们增加产品的业务逻辑
+            return orderService.manageSendGoods(orderNo);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+
+
+
+
+
+
+
+}
