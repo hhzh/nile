a669c3e843ae7cd34e178a4e13e2016f06cb3830
hhzh <1138253815@qq.com>
2017-05-31 19:31:26 +0800

    格式代码

--- src/main/java/com/nile/controller/backend/BookManageController.java
+++ src/main/java/com/nile/controller/backend/BookManageController.java
@@ -33,87 +33,87 @@ public class BookManageController {
     @Autowired
     private IBookService bookService;
     @Autowired
-    private IFileService iFileService;
+    private IFileService fileService;
 
     @RequestMapping("save")
     @ResponseBody
-    public ServerResponse bookSave(HttpSession session, Book book){
-        User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+    public ServerResponse bookSave(HttpSession session, Book book) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
 
         }
-        if(userService.checkAdminRole(user).isSuccess()){
+        if (userService.checkAdminRole(user).isSuccess()) {
             //填充我们增加产品的业务逻辑
             return bookService.saveOrUpdateBook(book);
-        }else{
+        } else {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
     }
 
     @RequestMapping("set_sale_status")
     @ResponseBody
-    public ServerResponse setSaleStatus(HttpSession session, Integer bookId,Integer status){
-        User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+    public ServerResponse setSaleStatus(HttpSession session, Integer bookId, Integer status) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
 
         }
-        if(userService.checkAdminRole(user).isSuccess()){
-            return bookService.setSaleStatus(bookId,status);
-        }else{
+        if (userService.checkAdminRole(user).isSuccess()) {
+            return bookService.setSaleStatus(bookId, status);
+        } else {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
     }
 
     @RequestMapping("detail")
     @ResponseBody
-    public ServerResponse getDetail(HttpSession session, Integer bookId){
-        User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+    public ServerResponse getDetail(HttpSession session, Integer bookId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
 
         }
-        if(userService.checkAdminRole(user).isSuccess()){
+        if (userService.checkAdminRole(user).isSuccess()) {
             //填充业务
             return bookService.manageBookDetail(bookId);
 
-        }else{
+        } else {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
     }
 
     @RequestMapping("list")
     @ResponseBody
-    public ServerResponse getList(HttpSession session, @RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
-                                  @RequestParam(value = "pageSize",defaultValue = "10") int pageSize){
-        User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+    public ServerResponse getList(HttpSession session, @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                  @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
 
         }
-        if(userService.checkAdminRole(user).isSuccess()){
+        if (userService.checkAdminRole(user).isSuccess()) {
             //填充业务
-            return bookService.getBookList(pageNum,pageSize);
-        }else{
+            return bookService.getBookList(pageNum, pageSize);
+        } else {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
     }
 
     @RequestMapping("search")
     @ResponseBody
-    public ServerResponse bookSearch(HttpSession session,String bookName,Integer bookId,
-                                     @RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
-                                     @RequestParam(value = "pageSize",defaultValue = "10") int pageSize){
-        User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+    public ServerResponse bookSearch(HttpSession session, String bookName, Integer bookId,
+                                     @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                     @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
 
         }
-        if(userService.checkAdminRole(user).isSuccess()){
+        if (userService.checkAdminRole(user).isSuccess()) {
             //填充业务
-            return bookService.searchBook(bookName,bookId,pageNum,pageSize);
-        }else{
+            return bookService.searchBook(bookName, bookId, pageNum, pageSize);
+        } else {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
     }
@@ -121,22 +121,22 @@ public class BookManageController {
     @RequestMapping("upload")
     @ResponseBody
     public ServerResponse upload(HttpSession session,
-                                 @RequestParam(value = "upload_file",required = false) MultipartFile file,
-                                 HttpServletRequest request){
-        User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+                                 @RequestParam(value = "upload_file", required = false) MultipartFile file,
+                                 HttpServletRequest request) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录管理员");
         }
-        if(userService.checkAdminRole(user).isSuccess()){
+        if (userService.checkAdminRole(user).isSuccess()) {
             String path = request.getSession().getServletContext().getRealPath("upload");
-            String targetFileName = iFileService.upload(file,path);
-            String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
+            String targetFileName = fileService.upload(file, path);
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
 
             Map fileMap = Maps.newHashMap();
-            fileMap.put("uri",targetFileName);
-            fileMap.put("url",url);
+            fileMap.put("uri", targetFileName);
+            fileMap.put("url", url);
             return ServerResponse.createBySuccess(fileMap);
-        }else{
+        } else {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
     }
@@ -145,13 +145,13 @@ public class BookManageController {
     @RequestMapping("richtext_img_upload")
     @ResponseBody
     public Map richtextImgUpload(HttpSession session,
-                                 @RequestParam(value = "upload_file",required = false) MultipartFile file,
-                                 HttpServletRequest request, HttpServletResponse response){
+                                 @RequestParam(value = "upload_file", required = false) MultipartFile file,
+                                 HttpServletRequest request, HttpServletResponse response) {
         Map resultMap = Maps.newHashMap();
-        User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
-            resultMap.put("success",false);
-            resultMap.put("msg","请登录管理员");
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            resultMap.put("success", false);
+            resultMap.put("msg", "请登录管理员");
             return resultMap;
         }
         //富文本中对于返回值有自己的要求,我们使用是simditor所以按照simditor的要求进行返回
@@ -160,53 +160,26 @@ public class BookManageController {
 //                "msg": "error message", # optional
 //            "file_path": "[real file path]"
 //        }
-        if(userService.checkAdminRole(user).isSuccess()){
+        if (userService.checkAdminRole(user).isSuccess()) {
             String path = request.getSession().getServletContext().getRealPath("upload");
-            String targetFileName = iFileService.upload(file,path);
-            if(StringUtils.isBlank(targetFileName)){
-                resultMap.put("success",false);
-                resultMap.put("msg","上传失败");
+            String targetFileName = fileService.upload(file, path);
+            if (StringUtils.isBlank(targetFileName)) {
+                resultMap.put("success", false);
+                resultMap.put("msg", "上传失败");
                 return resultMap;
             }
-            String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
-            resultMap.put("success",true);
-            resultMap.put("msg","上传成功");
-            resultMap.put("file_path",url);
-            response.addHeader("Access-Control-Allow-Headers","X-File-Name");
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+            resultMap.put("success", true);
+            resultMap.put("msg", "上传成功");
+            resultMap.put("file_path", url);
+            response.addHeader("Access-Control-Allow-Headers", "X-File-Name");
             return resultMap;
-        }else{
-            resultMap.put("success",false);
-            resultMap.put("msg","无权限操作");
+        } else {
+            resultMap.put("success", false);
+            resultMap.put("msg", "无权限操作");
             return resultMap;
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
