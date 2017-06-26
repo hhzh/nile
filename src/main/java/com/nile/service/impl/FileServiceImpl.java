8ec15f8eea74f0c6282c2c27c144ab414ea06bad
hhzh <1138253815@qq.com>
2017-06-26 21:31:32 +0800

    hold on

--- src/main/java/com/nile/service/impl/FileServiceImpl.java
+++ src/main/java/com/nile/service/impl/FileServiceImpl.java
@@ -34,7 +34,6 @@ public class FileServiceImpl implements IFileService {
         }
         File targetFile = new File(path, uploadFileName);
 
-        
         try {
             file.transferTo(targetFile);
             //文件已经上传成功了
