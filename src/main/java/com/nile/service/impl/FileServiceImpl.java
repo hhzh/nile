83e87ea7305ec79d6389b2114708d154514ccf7b
hhzh <1138253815@qq.com>
2017-07-01 21:20:59 +0800

    add

--- src/main/java/com/nile/service/impl/FileServiceImpl.java
+++ src/main/java/com/nile/service/impl/FileServiceImpl.java
@@ -17,7 +17,6 @@ public class FileServiceImpl implements IFileService {
 
     private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
 
-
     public String upload(MultipartFile file, String path) {
         String fileName = file.getOriginalFilename();
         
